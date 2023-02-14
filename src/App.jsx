2c0ab6e3c3a48f06448ff65df23b7fece68e7f7a
Sergio Guachalla <sergioguachalla0@gmail.com>
2023-02-14 11:19:23 -0400

    Segundo Commit y Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useEffect,useState } from "react"
 
 function App() {
 
+  const [criptos, setCriptos] = useState()
+
+
+  useEffect( () => {
+    fetch("https://api.coincap.io/v2/assets")
+    .then((resp) => resp.json())
+    .then((data) => {setCriptos(data.data)})
+    .catch(() => console.error("La petición falló"))
+  },[])
+  if(!criptos) return <h1>Cargando...</h1>
+
+
   return (
-    <h1>Hola Mundo</h1>
+    <>
+    <h1>Lista de criptomonedas</h1>
+    <ol>
+      { criptos.map(({name,priceUsd}) => (
+        <li>Nombre: {name} Precio: ${priceUsd} </li>
+      )
+      ) }
+    </ol>
+    </>
   )
+
+
 }
 
 export default App
