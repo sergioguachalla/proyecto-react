7b7bd6e65cd15c43e56950e128cdf5b9ee3d7808
Sergio Guachalla <sergioguachalla0@gmail.com>
2023-02-16 11:03:44 -0400

    React router added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './components/App'
 import './main.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Page404 from './components/404'
+import Cuadricula from './components/Cuadricula'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+   <>
+   
+   <BrowserRouter>
+      <Routes>
+         <Route path="/" element={<App/>}>
+            <Route index element={<Cuadricula/>}/>
+         </Route>
+         <Route path="*" element={<Page404/>}></Route>
+      </Routes>
+   </BrowserRouter>
+   </>
+   
+)
